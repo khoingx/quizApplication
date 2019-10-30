c8b86d0c40337171b13b16959ff0c43f8f80a476
khoingx <55979401+khoingx@users.noreply.github.com>
2019-10-30 15:36:22 +1100

    update question file

---
+++ question.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript:",
+        choices: ["<js>", "<javascript>", "<script>", "<scripting>"],
+        answer: "script"
+    },
+
+    {
+        title: "The external JavaScript file must contain the <script> tag.",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function myFunction()", "function:myFunction()"],
+        answer: "function = myFunction()"
+    }, 
+
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    
+  ];
+  
