97e04608d9e9ec3dcd921f44e27ff98ffef95423
khoingx <55979401+khoingx@users.noreply.github.com>
2019-11-04 18:24:25 +1100

    files added

---
+++ javascript.js
@@ -0,0 +1,64 @@
+const seconds = 60;
+let currentQuestionIndex = 0;
+const remainingSecondSpan = document.querySelector("#remaining-seconds")
+
+setInterval(function() {
+    remainingSecondSpan.textContent = seconds;
+    seconds--;
+}, 1000 );
+
+
+
+function getChoiceElement(choice) {
+    const element = document.createElement("div");
+
+}
+
+// const myQuestion = questions[0];
+
+function renderQuestion(questions) {
+    document.querySelector("#question").textContent = questions.title;
+
+    const choiceContainer = document.querySelector("#choices-container");
+
+    choiceContainer.innerHTML = '' ;
+
+    for (let index = 0; index < questions.choices.length; index++) {
+        const choice = questions.choices[index];
+        const element = document.createElement("div");
+        element.textContent = choice;
+        choiceContainer.appendChild(element);
+
+    }
+}
+
+const myQuestion = questions[0];
+
+renderQuestion(myQuestion);
+
+document.querySelector("#choices-container").addEventListener("click" , function(event) {
+
+    const target = event.target;
+
+    if (target.classList.contains("choice")) {
+        const answer = target.textContent;
+        const correctAnswer = myQuestion.answer;
+
+        if (answer.localeCompare(correctAnswer) === 0 ){
+            console.log('you are correct');
+        }
+        else {
+            console.log('you are wrong');
+        }
+
+        currentQuestionIndex++;
+        if (currentQuestionIndex > totalQuestionNumber) {
+            renderQuestion();
+
+        } else
+        {
+
+        }
+        renderQuestion();
+    }
+});
